af7acb0aede36ff7b148c28b502e85b6258dbdd2
Mateo Zaballo <mzaballo@hotmail.com>
2023-07-04 00:16:38 -0300

    Configuracion

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import List from "./Components/List/List";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <h1>TO-DO APP</h1>
+      <List></List>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
